f310c70cc68f285b6e2bb32bf6108808f0a39610
xueBaby <2193307353@qq.com>
2018-11-12 01:16:24 +0800

    users静态页面

--- src-02-todolist/main.js
+++ src-02-todolist/main.js
@@ -1,16 +1,13 @@
-
 /**
- * 主函数
+ * 入口文件js
  */
-
 import Vue from 'vue';
 import App from './App.vue';
 
 new Vue({
   el: '#app',
-  components: {
+  components:{
     App
   },
-  template:'<App />'
-  
+  template: '<App />' //需要把App渲染到页面 index.html上
 })
